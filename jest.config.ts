b9854f57893952870f446880e69818c1900052ec
romaleev <aromaleev@gmail.com>
2025-02-05 13:15:54 +0700

    lib ver updates, update eslint/jest configs, add tsconfig

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
 	verbose: true,
 	transform: {
-		'^.+\\.tsx?$': ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }]
+		'^.+\\.tsx?$': ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }],
 	},
 }
 export default config
