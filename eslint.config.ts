1f2d1e8a80568e8cd0ab44d5cbf88e7827f837f6
romaleev <aromaleev@gmail.com>
2025-02-05 13:31:23 +0700

    update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,17 +3,22 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default [
-	...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended),
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
 	eslintPluginPrettierRecommended,
 	{
-		rules: {
-			'eol-last': ['error', 'always'],
-		},
 		languageOptions: {
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
 			globals: {
 				...globals.node,
 			},
 		},
+		rules: {
+			'eol-last': ['error', 'always'], // Ensures newline at EOF
+		},
 	},
-]
+)
